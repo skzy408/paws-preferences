317c26d9f24841e7606f55e52490db7329e0a6f6
samkzy <skho0041@student.monash.edu>
2026-01-12 02:58:05 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/paws-preferences/',
+  base: '/paws-preferences/', // your repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // optional, default is dist
+    emptyOutDir: true
+  }
+});
