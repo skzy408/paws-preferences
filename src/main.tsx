b65de7fa62b1ca10f782e7dd4c53cc6d1ce05711
samkzy <skho0041@student.monash.edu>
2026-01-13 01:09:57 +0800

    UI changes - add dark theme toggle - favicon and tab title

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const media = window.matchMedia("(prefers-color-scheme: dark)");
+
+function applyTheme(isDark: boolean) {
+  document.documentElement.classList.toggle("dark", isDark);
+}
+
+applyTheme(media.matches);
+
+media.addEventListener("change", (e) => {
+  applyTheme(e.matches);
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
